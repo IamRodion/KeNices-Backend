23c83ec19c26a4673f9e12022d5eb5feb4c101f9
IamRodion <22568039+IamRodion@users.noreply.github.com>
2025-05-22 01:29:58 -0500

    Modifica el modelo Product Modifica registration_date del modelo Product para que se autogenere con la fecha del registro

--- api/models.py
+++ api/models.py
@@ -6,7 +6,7 @@ class Product(models.Model):
     price = models.DecimalField(max_digits=10, decimal_places=2)
     stock = models.PositiveIntegerField()
     provider = models.CharField(max_length=100)
-    registration_date = models.DateField(null=True, blank=True)
+    registration_date = models.DateField(auto_now_add=True)
     expiry_date = models.DateField(null=True, blank=True)
 
     def __str__(self):
