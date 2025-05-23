029a2f67699c48df600cd462beab3a6e4d6d4657
IamRodion <22568039+IamRodion@users.noreply.github.com>
2025-05-23 11:14:05 -0500

    Añade un filtro al endpoint /api/Product/ Añade el endpoint /api/Product/?ordering=expiry_date para poder filtrar los productos en orden de fecha de vencimiento

--- api/views.py
+++ api/views.py
@@ -1,4 +1,4 @@
-from rest_framework import viewsets, generics
+from rest_framework import viewsets, filters, generics
 # from rest_framework.permissions import AllowAny, IsAuthenticated
 from .models import Product, Sale, SaleItem
 from .serializers import ProductSerializer, SaleSerializer, SaleItemSerializer
@@ -7,6 +7,9 @@ from .serializers import ProductSerializer, SaleSerializer, SaleItemSerializer
 class ProductViewSet(viewsets.ModelViewSet):
     queryset = Product.objects.all()
     serializer_class = ProductSerializer
+    filter_backends = [filters.OrderingFilter]
+    ordering_fields = ['expiry_date']
+    ordering = ['expiry_date']
 
 class SaleViewSet(viewsets.ModelViewSet):
     queryset = Sale.objects.all()
